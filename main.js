f58a511911dadf5d383dee1324d182af1d81ccc4
huutai2002 <nhtai0560@gmail.com>
2023-04-26 18:27:11 +0700

    rut gon

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
-// -------------Subnav------------------
-const optionsDiv = document.querySelector("subnav");
-
-optionsDiv.addEventListener('mouseover', () => {
-  // Hiển thị các tùy chọn khi di chuột vào
-  optionsDiv.style.display = 'block';
-});
-
-optionsDiv.addEventListener('mouseout', () => {
-  // Ẩn các tùy chọn khi di chuột ra khỏi div
-  optionsDiv.style.display = 'none';
-});
-
-
-
-//-------------- Slider-----------------
-let slideIndex = 0;
+//-------------- SLIDER-----------------//
+let sliderIndex = 0;
 const slides = document.getElementsByClassName("slide")
 const slideCount = slides.length;
 const prevBtn = document.querySelector(".prevBtn");
@@ -22,31 +7,33 @@ const nextBtn = document.querySelector(".nextBtn");
 
 function showSlide(n) {
   if (n < 0) {
-    slideIndex = slideCount - 1;
+    sliderIndex = slideCount - 1;
   } else if (n >= slideCount) {
-    slideIndex = 0;
+    sliderIndex = 0;
   } else {
-    slideIndex = n;
+    sliderIndex = n;
   }
   for (let i = 0; i < slideCount; i++) {
     slides[i].style.opacity = "0";
   }
-  slides[slideIndex].style.opacity = "1";
+  slides[sliderIndex].style.opacity = "1";
 }
 
 function nextSlide() {
-  showSlide(slideIndex + 1);
+  showSlide(sliderIndex + 1);
 }
 
 function prevSlide() {
-  showSlide(slideIndex - 1);
+  showSlide(sliderIndex - 1);
 }
 
 nextBtn.addEventListener("click", nextSlide);
 prevBtn.addEventListener("click", prevSlide);
 
-setInterval(nextSlide, 5000);
-//-----------------Search-----------------------
+setInterval(nextSlide, 4000);
+//--------------END SLIDER-----------------//
+
+//-------------- SEARCH-----------------//
 function formValidate (){
   var frm= document.forms['regForm'];
   if (frm.search.value.length>0){
@@ -62,11 +49,12 @@ btn.addEventListener("keypress", function(event){
     }
 });
 
-
 var send=document.getElementById('gui');
 send.addEventListener("click", formValidate);
-//-----------------Lien he----------------------
-let searchForm = document.forms["frm-search"];
+//--------------END SEARCH-----------------//
+
+//-----------------LIEN HE----------------------//
+let searchForm = document.forms["form-search"];
 let searchButton = document.querySelector(".search-button");
 if (searchForm) {
     searchForm.onsubmit = (e) => { e.preventDefault(); };
@@ -78,3 +66,129 @@ if (searchForm) {
     searchForm.search.onkeypress = (e) => { if (e.which === 13) { result(e.target.value); } };
 }
 
+function result(keyword) {
+    if (keyword.length === 0) return;
+    searchForm.submit();
+}
+
+function frmValidateContact(frm) {
+    if (frm.name.value.length < 4) {
+        alert("Tên không hợp lệ !")
+        return false
+    }
+    let emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!emailReg.test(frm.email.value)) {
+        alert("Email không hợp lệ !")
+        return false
+    }
+    if (frm.feedback.value.length < 10) {
+        alert("Nội dung quá ngắn !")
+        return false;
+    }
+    alert("Gửi thành công")
+    return true;
+}
+//--------------END LIEN HE----------------------//
+
+//--------------DANG KY-DANG NHAP----------------//
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+});
+loginBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (()=>{
+  signupBtn.click();
+  return false;
+});
+
+
+function checkdk() { 
+  var email = document.getElementById('email').value; 
+  var password= document.getElementById('password').value;
+  var password1= document.getElementById('password1').value;
+  var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/; //bieu thuc chinh quy
+  if(email=='' && password=='' && password1=='')
+      alert('Vui lòng nhập email và password !')
+  if (!filter.test(email) || email =='') { 
+      alert('Email không hợp lệ.');
+      email.focus; 
+      return false; 
+  }
+  else if (password=='' || password.length < 8){
+      alert('Password không hợp lệ.');
+      return false; 
+  }
+  else if (password1=='' || password.length < 8 || password1 != password){
+      alert('Password không khớp.');
+      return false; 
+  }
+  else
+      alert('OK. Đăng ký thành công.'); 
+} 
+
+
+function checkdn() { 
+  var email = document.getElementById('email').value; 
+  var password= document.getElementById('password').value;
+  var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/; 
+  if(email=='' && password=='')
+      alert('Vui lòng nhập email và password !')
+  if (!filter.test(email) || email =='') { 
+      alert('Email không hợp lệ.');
+      email.focus; 
+      return false; 
+  }
+  else if (password=='' || password.length < 8){
+      alert('Password không hợp lệ.');
+      return false; 
+  }
+  else{
+      alert('OK. Đăng nhập thành công.'); 
+      window.open("./trangchu.html");
+      window.open("./dangnhap.html");
+  }
+
+}
+//--------------END DANG KY-DANG NHAP----------------//
+
+// ------------SLIDER CHINH SACH------------------//
+//khai báo biến slideIndex đại diện cho slide hiện tại
+var slideIndex;
+// KHai bào hàm hiển thị slide
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex].style.display = "block";  
+    dots[slideIndex].className += " active";
+    //chuyển đến slide tiếp theo
+    slideIndex++;
+    //nếu đang ở slide cuối cùng thì chuyển về slide đầu
+    if (slideIndex > slides.length - 1) {
+      slideIndex = 0
+    }    
+    //tự động chuyển đổi slide sau 5s
+    setTimeout(showSlides, 5000);
+}
+//mặc định hiển thị slide đầu tiên 
+showSlides(slideIndex = 0);
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+// ------------------------------------------
